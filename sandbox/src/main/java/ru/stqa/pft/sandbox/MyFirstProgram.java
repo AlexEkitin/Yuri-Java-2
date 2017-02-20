6ba1f63a244ce2ba5044e36234dcb5cd93e83f61
AlexEkinin <kiluy9iuoh@yandex.ru>
2017-02-20 11:29:50 -0800

    1.14

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,6 +2,25 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
    public static void main(String[] args) {
-      System.out.println("Hello, world!");
+      hello("world");
+      hello("Yuri");
+
+      Square s = new Square(5);
+      System.out.println(area(s));
+
+      Rectangle r = new Rectangle(4, 6);
+      System.out.println(area(r));
+   }
+
+   public static void hello(String somebody) {
+      System.out.println("Hello, " + somebody + "!");
+   }
+
+   public static double area(Square s) {
+      return s.l * s.l;
+   }
+
+   public static double area(Rectangle r) {
+      return r.a * r.b;
    }
 }
